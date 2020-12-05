54507d39151b1c89b7ec1b9a673f6f0cdb7306e1
Mathis Hamelotte <mathis.hamelotte@etudiant.univ-lr.fr>
2020-12-05 21:42:51 +0100

    fichiers pour la base de donnée + comment mettre en place sur discord

---
+++ FiFruitEtLegume/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,203 @@
+<?php
+
+namespace App\DataFixtures;
+
+
+use App\Entity\Producteur;
+use App\Entity\Produit;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $Producteur1=new Producteur();
+        $Producteur1->setPrenom('Mathis');
+        $Producteur1->setNom('Hamelotte');
+        $Producteur1->setNomDeLEntreprise('PetitPotager');
+        $Producteur1->setDescription('jeune producteur, je souhaite vous faire découvrir ma passion à l"aide de mes produits cultivers avec soins.');
+        $Producteur1->setAdresse('3 rue des accacias, 17430 Saint-Hippolyte');
+        $Producteur1->setTelephone('0662378055');
+        $Producteur1->setPhoto('grathis');
+        
+        $manager->persist($Producteur1);
+
+        $Producteur2= new Producteur();
+        $Producteur2->setPrenom('Romain');
+        $Producteur2->setNom('Durieux');
+        $Producteur2->setNomDeLEntreprise('FiFruit');
+        $Producteur2->setDescription('salut c"est Romain. salut c"est Romain. salut c"est Romain. salut c"est Romain. salut c"est Romain. salut c"est Romain. salut c"est Romain. salut c"est Romain. ');
+        $Producteur2->setAdresse('6 rue des fifruit, 17000 La Rochelle');
+        $Producteur2->setTelephone('0600000001');
+        $Producteur2->setPhoto('grathis');
+
+        $manager->persist($Producteur2);
+
+        $Producteur3=new Producteur();
+        $Producteur3->setPrenom('Phillipe');
+        $Producteur3->setNom('Roux');
+        $Producteur3->setNomDeLEntreprise('FiLegume');
+        $Producteur3->setDescription('Aucune description');
+        $Producteur3->setAdresse('9 rue des filegume, 17300 Rochefort');
+        $Producteur3->setTelephone('0600000002');
+        $Producteur3->setPhoto('grathis');
+
+        $manager->persist($Producteur3);
+
+        $Producteur4=new Producteur();
+        $Producteur4->setPrenom('Gael');
+        $Producteur4->setNom('Mousset');
+        $Producteur4->setNomDeLEntreprise('Pitager');
+        $Producteur4->setDescription('Aucune description');
+        $Producteur4->setAdresse('8 rue Charles de Gaulle, 17440 Aytré');
+        $Producteur4->setTelephone('0600000003');
+        $Producteur4->setPhoto('grathis');
+
+        $manager->persist($Producteur4);
+
+        $Producteur5=new Producteur();
+        $Producteur5->setPrenom('Nhan');
+        $Producteur5->setNom('HO Dinh Huu');
+        $Producteur5->setNomDeLEntreprise('Potiger');
+        $Producteur5->setDescription('Aucune description');
+        $Producteur5->setAdresse('8 rue Louis Pasteur, 17320 Marenne');
+        $Producteur5->setTelephone('0600000004');
+        $Producteur5->setPhoto('grathis');
+
+        $manager->persist($Producteur5);
+
+        $Producteur6=new Producteur();
+        $Producteur6->setPrenom('Lea');
+        $Producteur6->setNom('Bisot');
+        $Producteur6->setNomDeLEntreprise('Fique');
+        $Producteur6->setDescription('Aucune description');
+        $Producteur6->setAdresse('3 rue des accacias, 17200 Royan');
+        $Producteur6->setTelephone('0600000005');
+        $Producteur6->setPhoto('grathis');
+
+        $manager->persist($Producteur6);
+
+        $Producteur7=new Producteur();
+        $Producteur7->setPrenom('julie');
+        $Producteur7->setNom('Bouchet');
+        $Producteur7->setNomDeLEntreprise('LaVache');
+        $Producteur7->setDescription('Aucune description');
+        $Producteur7->setAdresse('3 rue des accacias, 17100 Sainte');
+        $Producteur7->setTelephone('0600000006');
+        $Producteur7->setPhoto('grathis');
+
+        $manager->persist($Producteur7);
+
+        $Producteur8=new Producteur();
+        $Producteur8->setPrenom('Matthieu');
+        $Producteur8->setNom('Decroix');
+        $Producteur8->setNomDeLEntreprise('BioUnivers');
+        $Producteur8->setDescription('Aucune description');
+        $Producteur8->setAdresse('3 rue Jules Ferry, 17430 Tonnay-Charente');
+        $Producteur8->setTelephone('0600000007');
+        $Producteur8->setPhoto('grathis');
+
+        $manager->persist($Producteur8);
+
+        $Producteur9=new Producteur();
+        $Producteur9->setPrenom('Jean');
+        $Producteur9->setNom('Neveu');
+        $Producteur9->setNomDeLEntreprise('Restaurant Ambroisia');
+        $Producteur9->setDescription('Aucune description');
+        $Producteur9->setAdresse('17 Avenue Charles de Gaulle, 17450 Fouras');
+        $Producteur9->setTelephone('0600000008');
+        $Producteur9->setPhoto('grathis');
+
+        $manager->persist($Producteur9);
+
+        $Producteur10=new Producteur();
+        $Producteur10->setPrenom('Gaspard');
+        $Producteur10->setNom('Dubois');
+        $Producteur10->setNomDeLEntreprise('LegumeBio');
+        $Producteur10->setDescription('Aucune description');
+        $Producteur10->setAdresse('3 rue Léon Gambetta, 17138 Saint-Xandre');
+        $Producteur10->setTelephone('0600000009');
+        $Producteur10->setPhoto('grathis');
+
+        $manager->persist($Producteur10);
+
+        $Producteur11=new Producteur();
+        $Producteur11->setPrenom('Audrey');
+        $Producteur11->setNom('Lassalle');
+        $Producteur11->setNomDeLEntreprise('FruitBio');
+        $Producteur11->setDescription('Aucune description');
+        $Producteur11->setAdresse('3 rue Victor Hugo, 17620 Echillais');
+        $Producteur11->setTelephone('0600000010');
+        $Producteur11->setPhoto('grathis');
+
+        $manager->persist($Producteur11);
+
+
+
+        //produit
+        $Produit1=new Produit();
+        $Produit1->setNom('Carotte');
+        $Produit1->setDescription('Carotte cultiver dans mon potager');
+        $Produit1->setType('legume');
+        $Produit1->setQuantite('12');
+        $Produit1->setPrix('3');
+        $Produit1->setProducteur($Producteur1);
+
+        $manager->persist($Produit1);
+
+        $Produit2=new Produit();
+        $Produit2->setNom('concombre');
+        $Produit2->setDescription('Aucune description');
+        $Produit2->setType('legume');
+        $Produit2->setQuantite('13');
+        $Produit2->setPrix('5');
+        $Produit2->setProducteur($Producteur1);
+
+        $manager->persist($Produit2);
+
+        $Produit3=new Produit();
+        $Produit3->setNom('oignon rouge');
+        $Produit3->setDescription('Aucune description');
+        $Produit3->setType('legume');
+        $Produit3->setQuantite('5');
+        $Produit3->setPrix('5');
+        $Produit3->setProducteur($Producteur1);
+
+        $manager->persist($Produit3);
+
+        $Produit4=new Produit();
+        $Produit4->setNom('aubergine');
+        $Produit4->setDescription('Aucune description');
+        $Produit4->setType('legume');
+        $Produit4->setQuantite('5');
+        $Produit4->setPrix('5');
+        $Produit4->setProducteur($Producteur1);
+
+        $manager->persist($Produit4);
+
+        $Produit5=new Produit();
+        $Produit5->setNom('fraise');
+        $Produit5->setDescription('Aucune description');
+        $Produit5->setType('fruit');
+        $Produit5->setQuantite('9');
+        $Produit5->setPrix('2');
+        $Produit5->setProducteur($Producteur1);
+
+        $manager->persist($Produit5);
+
+        $Produit6=new Produit();
+        $Produit6->setNom('cabillaud');
+        $Produit6->setDescription('Aucune description');
+        $Produit6->setType('poisson');
+        $Produit6->setQuantite('2');
+        $Produit6->setPrix('15');
+        $Produit6->setProducteur($Producteur2);
+
+        $manager->persist($Produit6);
+
+
+
+        $manager->flush();
+    }
+}
---
+++ FiFruitEtLegume/src/Entity/Producteur.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProducteurRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProducteurRepository::class)
+ */
+class Producteur
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=60)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=60)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $nom_de_l_entreprise;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adresse;
+
+    /**
+     * @ORM\Column(type="string", length=13, nullable=true)
+     */
+    private $telephone;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Produit::class, mappedBy="producteur")
+     */
+    private $produits;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $photo;
+
+    public function __construct()
+    {
+        $this->produits = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getNomDeLEntreprise(): ?string
+    {
+        return $this->nom_de_l_entreprise;
+    }
+
+    public function setNomDeLEntreprise(?string $nom_de_l_entreprise): self
+    {
+        $this->nom_de_l_entreprise = $nom_de_l_entreprise;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(?string $telephone): self
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Produit[]
+     */
+    public function getProduits(): Collection
+    {
+        return $this->produits;
+    }
+
+    public function addProduit(Produit $produit): self
+    {
+        if (!$this->produits->contains($produit)) {
+            $this->produits[] = $produit;
+            $produit->setProducteur($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduit(Produit $produit): self
+    {
+        if ($this->produits->removeElement($produit)) {
+            // set the owning side to null (unless already changed)
+            if ($produit->getProducteur() === $this) {
+                $produit->setProducteur(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(?string $photo): self
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+}
