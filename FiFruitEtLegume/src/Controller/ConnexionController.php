4ec9f0c92b9f1d066d9ee7185691ecef5ee23b89
rdurieux <romain.durieux@etudiant.univ-lr.fr>
2021-01-13 10:50:50 +0100

    [ADD] Route vers la page Inscription

--- FiFruitEtLegume/src/Controller/ConnexionController.php
+++ FiFruitEtLegume/src/Controller/ConnexionController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Form\InscriptionFormType;
+use App\Entity\Consommateur;
+
 class ConnexionController extends AbstractController
 {
     /**
@@ -14,6 +17,20 @@ class ConnexionController extends AbstractController
     {
         return $this->render('Connexion/connexion.html.twig', [
             "name" => "Inscription"
+        ]);
+    }
+
+    /**
+     * @Route("/inscription", name="inscription")
+     */
+    public function add()
+    {
+        $consommateur = new Consommateur();
+        $form = $this->createForm(InscriptionFormType::class, $consommateur);
+
+        return $this->render('Connexion/connexion.html.twig', [
+            "name" => "Inscription" ,
+            'form' => $form->createView()
         ]);
     }
 }
