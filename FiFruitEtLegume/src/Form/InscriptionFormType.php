d50fb0b308b8be836845c440da420870ce46d241
rdurieux <romain.durieux@etudiant.univ-lr.fr>
2021-01-13 10:53:34 +0100

    [ADD] Formulaire d'inscription

---
+++ FiFruitEtLegume/src/Form/InscriptionFormType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Consommateur;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InscriptionFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('adresse', TextType::class)
+            ->add('telephone', TextType::class)
+            ->add('photo', TextType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Consommateur::class,
+        ]);
+    }
+}
