b63e54fb31b864571f9741da85f13957623ad425
Romain Durieux <romain.durieux@etudiant.univ-lr.fr>
2021-01-17 23:36:17 +0100

    [ADD][FIX][Gael] "merge" controller carte

---
+++ FiFruitEtLegume/src/Controller/CarteController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Producteur;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CarteController extends AbstractController
+{
+    /**
+     * @Route("/carte", name="carte")
+     */
+    public function index(): Response
+    {
+
+        $repositoryProducteur = $this->getDoctrine()->getManager()->getRepository("App\Entity\Producteur");
+        $producteurs = $repositoryProducteur->findAll();
+        return $this->render('carte/index.html.twig', [
+            'controller_name' => 'CarteController',
+            //'data' => $fake_data,
+            'data' =>$producteurs
+
+        ]);
+    }
+}
